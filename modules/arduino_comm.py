8fae55ea1df6c244abaa085cf3c5c025c56c588a
S McIntyre <sdamcintyre+git@gmail.com>
2021-12-07 17:59:46 +0100

    update to pulses

--- modules/arduino_comm.py
+++ modules/arduino_comm.py
@@ -1,8 +1,7 @@
 import time
 
-import serial
-
 from libraries.triggerbox import TriggerBox
+from psychopy import core
 
 # Interact with arduino connected through USB port
 
@@ -11,17 +10,23 @@ class ArduinoComm:
 
     def __init__(self):
         self.trigger = TriggerBox()
-        self.pulse = self.trigger.make_analog_signal(channel=3, voltage=5, duration=20)  # in ms
+        self.pulse_duration = 70 # in ms
+        self.pulse = self.trigger.make_analog_signal(channel=3, voltage=5, duration=self.pulse_duration)  # in ms
         self.running = self.trigger.make_analog_signal(channel=3, voltage=5, duration=0)  # infinite
-        self.stop = self.trigger.make_analog_signal(channel=3, voltage=0, duration=0)  # infinite
+        self.stop = self.trigger.make_cancel_signal(channel=3)  # stop ch 3
+        self.timer = core.Clock()
 
         self.trigger.ser.write(self.stop)
 
     def send_pulses(self, nb_pulse):
 
-        for current_pulse in range(1, nb_pulse):
+        for current_pulse in range(nb_pulse):
+            self.timer.reset()
             self.trigger.ser.write(self.pulse)
-            time.sleep(0.040)  # Sleep for 20 milliseconds
+            while self.timer.getTime() < self.pulse_duration/1000*2:
+                pass
+
+        #time.sleep(0.175)  # Sleep for 70 milliseconds
 
         self.trigger.ser.write(self.running)
 
